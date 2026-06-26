757097014f443649af2b811cc7200f6119b6bafd
arshsiddique9 <arshhbibi18@gmail.com>
2026-06-26 21:27:39 +0530

    Update ProductGrid.tsx

--- components/ProductGrid.tsx
+++ components/ProductGrid.tsx
@@ -1,8 +1,12 @@
+import ProductCard from "./ProductCard";
 import { Product } from "@/lib/products";
+
 export default function ProductGrid({ products }: { products: Product[] }) {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-6">
-      {products.map((p) => <ProductCard key={p.id} p={p} />)}
+      {products.map((p) => (
+        <ProductCard key={p.id} p={p} />
+      ))}
     </div>
   );
 }
